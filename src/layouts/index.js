30f45d02b8e01a7ccbae8c91a2c51634ef26e272
doodybrains <doodybrainz@gmail.com>
2018-09-03 22:25:01 -0400

    fix

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import './reset.scss'
-import './index.css'
 
 const Layout = ({ children, data }) => (
   <div>
