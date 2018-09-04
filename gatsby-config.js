89cad6ba6ba9317765bd04383816d7fc93d58039
doodybrains <doodybrainz@gmail.com>
2018-09-03 23:07:46 -0400

    edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 
 module.exports = {
   siteMetadata: {
-    title: 'e rae ~ ITP Doc Blog :)',
+    title: 'e rae ~ ITP doc blog :)',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
