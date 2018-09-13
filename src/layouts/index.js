0440d3e61934bb502ed59694c39fca875eda3339
doodybrains <doodybrainz@gmail.com>
2018-09-13 12:59:36 -0400

    Add favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import './reset.scss'
 import '../assets/stylesheets/base.scss'
+import favicon from '../assets/favicon.ico'
 
 const Layout = ({ children, data }) => (
   <div>
     <Helmet>
       <html lang="en" />
       <title>{data.site.siteMetadata.title}</title>
-      <link rel="icon" type="image/png" href="assets/favicon.ico" sizes="16x16" />
-
+      <link rel="icon" href={favicon} />
       <link href="https://fonts.googleapis.com/css?family=Overpass|Overpass+Mono" rel="stylesheet"/>
     </Helmet>
 
